70f7545b4028a6d6a1c230c7a7f876d82072d603
nya282828 <cat.route20@gmail.com>
2016-01-10 21:23:39 +0900

    jsファイル叩き

---
+++ js/top.js
@@ -0,0 +1,18 @@
+var opAnimation = {
+
+	animation: function(){
+		$('js-load-bg')
+	}
+
+	init: function () {
+		// body...
+	}
+}
+
+$(window).load(function (){
+	$('.loading').hide();
+})
+
+$(document).ready(function () {
+	opAnimation.init();
+});
